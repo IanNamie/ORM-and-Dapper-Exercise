0696396e0437b5e65f5a56efb1ff33421e1fc67d
Ian Namie <Iannamie82@gmail.com>
2023-07-13 21:11:38 -0500

    finished exercise 1

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+	public class DapperDepartmentRepository : IDepartmentRepository
+	{
+		private readonly IDbConnection _conn;
+
+		public DapperDepartmentRepository(IDbConnection conn)
+		{
+			_conn = conn;
+		}
+
+		public IEnumerable<Department> GetAllDepartments()
+		{
+			string query = "SELECT * FROM departments";
+			return _conn.Query<Department>(query);
+		}
+
+		public void InsertDepartments(string name)
+		{
+			_conn.Execute("INSERT INTO Department (name) VALUES (@name)", new { name });
+		}
+	}
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Net.Http.Headers;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			string connString = config.GetConnectionString("DefaultConnection");
+
+			IDbConnection conn = new MySqlConnection(connString);
+
+
+			var DepartmentRepo = new DapperDepartmentRepository(conn);
+			var Departments = DepartmentRepo.GetAllDepartments();
+
+			foreach (var department in Departments) 
+			{
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+		}
     }
 }
